8e98fe22d22f58522ecad79cc14bebc88b6916f7
VISHALUNNI <vishalunni87@gmail.com>
2023-08-03 21:10:06 +0530

    Permanent and Present Address Form added

--- src/assets/Components/Page.jsx
+++ src/assets/Components/Page.jsx
@@ -93,6 +93,42 @@ const Page =()=>{
                 </div>
                 </form>
             </div>
+            <div className="PermenantAddress">
+                <h3>Permanent Address: </h3>
+                <form className="PermenantAddress">
+                    <div className="PermenantAddress">
+                        <label className='Address'>Permanent Address : </label>
+                        <input type="text" className="PermenantAddress" placeholder="Address" maxLength="40"></input>
+                        <br></br>
+                        <label className='Pin'>Pin Code : </label>
+                        <input type="text" className="PermenantPinCode" placeholder="PIN" maxLength={7}></input>
+                        <br></br>
+                        <label className='District'>District : </label>
+                        <input type="text" className="PermenantDistrict" placeholder="District" maxLength={30}></input>
+                        <br></br>
+                        <label className='State'>State : </label>
+                        <input type="text" className="State" placeholder="State" maxLength={30}></input>
+                    </div>
+                </form>
+            </div>
+            <div className="PresentAddress">
+                <h3>Present Address : </h3>
+                <form className="PresentAddressform">
+                    <div className="presentAddress">
+                        <label className='address'>Present Address : </label>
+                        <input  type="text" className="PresentAddress" placeholder="Address"></input>
+                        <br></br>
+                        <label className='Pin'>Pin Code : </label>
+                        <input type="text" className="PermenantPinCode" placeholder="PIN" maxLength={7}></input>
+                        <br></br>
+                        <label className='District'>District : </label>
+                        <input type="text" className="PermenantDistrict" placeholder="District" maxLength={30}></input>
+                        <br></br>
+                        <label className='State'>State : </label>
+                        <input type="text" className="State" placeholder="State" maxLength={30}></input>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
