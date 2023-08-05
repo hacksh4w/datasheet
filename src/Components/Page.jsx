2fb97a89c2c6996faa9cd5112fa6a5b6e71be194
hacksh4w <mrinalsnair@gmail.com>
2023-08-05 06:34:28 +0530

    refactor

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -73,24 +73,24 @@ const Page =()=>{
             </div>
             <div className="Fathers/Guardian-details">
                 <form className="Fathers/Guardian-details">
-                <h3>Fathers/Guardians Details:</h3>
-                <div className='Name-Occupation'>
-                    <label className='Name'>Name: </label>  
-                    <input type="text" className="name" placeholder="Name"maxLength={30}></input>
-                    &nbsp;&nbsp;&nbsp;
-                    <label className='Occupation'>Occupation: </label>
-                    <input type="text" className="occupation" placeholder="Occupation" maxLength={30}></input>
-                </div>
-                <div className="email-phone">
-                    <label className='email'>Email: </label>
-                    <input type="email" className="email" placeholder="Parent's Email" maxLength={50}></input>
-                    &nbsp;&nbsp;&nbsp;
-                    <label className='Phone' placeholder="Phone">Phone Number 1 : </label>
-                    <input type="number" className="phone" placeholder="Phone Number" maxLength={15}></input>
-                    &nbsp;&nbsp;&nbsp;
-                    <label className='Phone' placeholder="Phone">Phone Number 2 : </label>
-                    <input type="number" className="phone" placeholder="Phone Number" maxLength={15}></input>
-                </div>
+                    <h3>Fathers/Guardians Details:</h3>
+                    <div className='Name-Occupation'>
+                        <label className='Name'>Name: </label>  
+                        <input type="text" className="name" placeholder="Name"maxLength={30}></input>
+                        &nbsp;&nbsp;&nbsp;
+                        <label className='Occupation'>Occupation: </label>
+                        <input type="text" className="occupation" placeholder="Occupation" maxLength={30}></input>
+                    </div>
+                    <div className="email-phone">
+                        <label className='email'>Email: </label>
+                        <input type="email" className="email" placeholder="Parent's Email" maxLength={50}></input>
+                        &nbsp;&nbsp;&nbsp;
+                        <label className='Phone' placeholder="Phone">Phone Number 1 : </label>
+                        <input type="number" className="phone" placeholder="Phone Number" maxLength={15}></input>
+                        &nbsp;&nbsp;&nbsp;
+                        <label className='Phone' placeholder="Phone">Phone Number 2 : </label>
+                        <input type="number" className="phone" placeholder="Phone Number" maxLength={15}></input>
+                    </div>
                 </form>
             </div>
             <div className="PermenantAddress">
@@ -176,7 +176,7 @@ const Page =()=>{
                             <option>Electronics And Communication Engineering(EC)</option>
                             <option>Electrical And Electronics Engineering(EEE)</option>
                             <option>Electronics(VLSI Design and Technology)</option>
-                            <option>Electronics And Bio-Medical(EB)</option>
+                            <option>Electronics And Bio-Medical Engineering(EB)</option>
                             <option>Mechanical Engineering(ME)</option>
                         </select>
                     </div>
